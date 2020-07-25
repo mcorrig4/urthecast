a1a1576a47512c2a5018e906782759498047fd4f
Liam Corrigan <liam.corr@gmail.com>
2020-07-25 00:59:23 -0400

    adding problems from glassdoor questions

---
+++ CalcFib/ConsoleApp1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I will calculate the nth Fibonacci number.");
+            int n = GetN();
+            int f = CalcFib(n);
+            Console.WriteLine("The {0}th Finonacci number is: {1}", n, f);
+            Console.WriteLine("");
+            Console.Write("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Keeps asking for console input until a valid non-neagative number is provided. This recusive function does not use any while or for loops
+        /// </summary>
+        /// <returns></returns>
+        private static int GetN()
+        {
+            Console.Write("Enter value for n: ");
+            string l = Console.ReadLine();
+            int n;
+            if (Int32.TryParse(l, out n))
+            {
+                if (n >= 0)
+                {
+                    return n;
+                }
+            }
+            Console.WriteLine("Invalid input!\nn must be a non-negative number.\nPlease try again.");
+            return GetN();
+        }
+
+        /// <summary>
+        /// Calculates nth Fibonacci number. {0, 1, 1, 2, 3, 5, 8, 13, ...}
+        /// </summary>
+        /// <param name="n">Value for n to calculate. Must be non-negative</param>
+        /// <returns>nth Fibonacci number</returns>
+        private static int CalcFib(int n)
+        {
+            if (n < 0)
+            {
+                throw new Exception("n must be >= 0");
+            }
+
+            // 0, 1, 1, 2, 3, 5, 8, 13, ...
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+            else
+            {
+                return CalcFib(n - 1) + n;
+            }
+        }
+    }
+}
---
+++ ImmutableList/ImmutableList/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ImmList
+{
+    class Program
+    {
+        //Write an immutable function that merges the following inputs into a single list. (Feel free to use the space below or submit a link to your work.)
+        //Inputs
+        //- Original list of strings
+        //- List of strings to be added
+        //- List of strings to be removed
+        //Return
+        //- List shall only contain unique values
+        //- List shall be ordered as follows
+        //--- Most character count to least character count
+        //--- In the event of a tie, reverse alphabetical
+        //Other Notes
+        //- You can use any programming language you like
+        //- The function you submit shall be runnable
+        //For example:
+        //Original List = ['one', 'two', 'three',]
+        //Add List = ['one', 'two', 'five', 'six]
+        //Delete List = ['two', 'five']
+        //Result List = ['three', 'six', 'one']
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private static ImmutableList<string> ChangeList(ImmutableList<string> original, ImmutableList<string> add, ImmutableList<string> remove)
+        {
+            List <string> temp = new List<string>(original);
+
+            temp.AddRange(add);
+
+            var rm = new HashSet<string>(remove);
+            temp.RemoveAll(str => rm.Contains(str));
+
+            temp.Sort(CompareItems);
+
+            return ImmutableList.Create(temp.ToArray());
+        }
+
+
+        /// <summary>
+        /// Make a method for comparing the list items based on the specified rules.
+        /// - List shall be ordered as follows
+        /// --- Most character count to least character count
+        /// --- In the event of a tie, reverse alphabetical
+        /// </summary>
+        private static int CompareItems(string item1, string item2)
+        {
+            // If other is not a valid object reference, this instance is greater.
+            if (item2 == null)
+            {
+                if (item1 == null)
+                    return 1;
+                else 
+                    return 1;
+            }
+
+            //- List shall be ordered as follows
+            //--- Most character count to least character count
+            //--- In the event of a tie, reverse alphabetical
+            if (item1.Length == item2.Length)
+            {
+                return item1.CompareTo(item2) * -1; //swap to reverse order by multiplying by neg 1
+            }
+            else
+            {
+                return item1.Length.CompareTo(item2.Length) * -1; // reverse order
+            }
+        }
+
+    }
+}
